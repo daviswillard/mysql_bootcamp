b748f067a4a57de0d8fe4bfd02ee16f5617cf026
daviswillard <kmlzntlln1@gmail.com>
2023-01-21 17:55:36 +0300

    day00

---
+++ day00/srcs/ex01/day00_ex01.sql
@@ -0,0 +1,1 @@
+select name, age from sql_bootcamp.person where address = 'Kazan' and gender = 'female' order by name;
---
+++ day00/srcs/ex02/day00_ex02.sql
@@ -0,0 +1,3 @@
+select name, rating from sql_bootcamp.pizzeria where rating >= 3.5 and rating <= 5;
+
+select name, rating from sql_bootcamp.pizzeria where rating between 3.5 and 5;
